7d49cc554e84e173f42e99a4d2366e50fc3e9907
John MacLean <tribute2johnny@gmail.com>
2022-09-20 13:04:20 +0100

    first commit

---
+++ src/test/java/BearTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BearTest {
+    Bear bear;
+
+    @Before
+    public void before(){
+       bear = new Bear("Baloo", 25, 95.62);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Baloo", bear.getName());
+    }
+
+    @Test
+    public void hasAge(){
+        assertEquals(25, bear.getAge());
+    }
+
+    @Test
+    public void hasWeight(){
+        assertEquals(95.62, bear.getWeight(), 0.0);
+    }
+}
